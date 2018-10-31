8d03524af22893380ece5079c50e619d59f1a73d
Daniel Fürth <admin@danielfurth.com>
2018-10-31 13:00:50 -0400

    map to rat

---
+++ map_to_rat.R
@@ -0,0 +1,47 @@
+map.to.rat<-function(dataset, regi, col = 'lightblue'){
+  back.warp<-stereotactic.coordinates(dataset$ML, dataset$DV, regi, inverse =TRUE)
+  
+  scale.factor <- mean(dim(regi$transformationgrid$mx)/c(regi$transformationgrid$height, 
+                                                         regi$transformationgrid$width))
+  xMax <- max(c(regi$transformationgrid$mx, regi$transformationgrid$mxF), 
+              na.rm = TRUE) * (1/scale.factor)
+  xMin <- min(c(regi$transformationgrid$mx, regi$transformationgrid$mxF), 
+              na.rm = TRUE) * (1/scale.factor)
+  yMax <- max(c(regi$transformationgrid$my, regi$transformationgrid$myF), 
+              na.rm = TRUE) * (1/scale.factor)
+  yMin <- min(c(regi$transformationgrid$my, regi$transformationgrid$myF), 
+              na.rm = TRUE) * (1/scale.factor)
+  
+  plot(c(xMin, xMax), c(yMin, yMax), ylim = c(yMax, yMin), 
+       xlim = c(xMin, xMax), asp = 1, axes = F, xlab = "", ylab = "", col=0)
+  
+  line.type <- c(3, 1)[as.integer(regi$atlas$col == "#cccccc") + 1]
+  line.type[1]<-1
+  line.width<-rep(1, length(line.type))
+  line.width[1]<-3
+  fill.col <- c(gray(0.95), gray(0.85))[as.integer(regi$atlas$col == "#cccccc") + 1]
+  fill.col[which(regi$atlas$col == "#aaaaaa")]<-"black"
+  
+  lapply(1:regi$atlas$numRegions, function(x) {
+    polygon(regi$atlas$outlines[[x]]$xrT/scale.factor, 
+            regi$atlas$outlines[[x]]$yrT/scale.factor, 
+            border = 'black', lty = line.type[x], col = fill.col[x], lwd = line.width[x])
+  })
+  lapply(1:regi$atlas$numRegions, function(x) {
+    polygon(regi$atlas$outlines[[x]]$xlT/scale.factor, 
+            regi$atlas$outlines[[x]]$ylT/scale.factor, 
+            border = 'black', lty = line.type[x], col = fill.col[x], lwd = line.width[x] )
+  })
+  
+  
+  
+  index <- cbind(as.integer(round(back.warp$y)), as.integer(round(back.warp$x)))
+  index<-index[-which(index<0),]
+  index[,2]<- (index[,2]+ regi$centroidNorm[2])
+  index[,1]<- (index[,1]- regi$centroidNorm[1])
+  xT <- (index[,2] + (regi$transformationgrid$mx[index] - index[,2]))
+  yT <- (index[,1] + (regi$transformationgrid$my[index] - index[,1]))
+  
+  points(xT, yT, pch=16, cex=0.2, col= col)
+  
+}
